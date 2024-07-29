1b5eed2cc6c68cbaceb201ed82e5d68e74d3eefc
Laura Alves Santana <164670475+LauraResan@users.noreply.github.com>
2024-07-29 15:15:22 -0300

    atividade

---
+++ aula13/Ambiente.js
@@ -0,0 +1,13 @@
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+var c = 1
+while (c <= 6) {
+    console.log('tudo bem?')
+    c++ // mesma coisa que c=c+1
+}
