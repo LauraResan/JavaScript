e5cf4d32a092dafc358f62c0652ed116d1d7bbea
Laura Alves Santana <164670475+LauraResan@users.noreply.github.com>
2024-07-29 15:22:55 -0300

    atividade

--- aula13/Ambiente.js
+++ aula13/Ambiente.js
@@ -1,4 +1,4 @@
-/*
+/* feito de forma bruta
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
@@ -7,7 +7,19 @@ console.log('Tudo bem?')
 console.log('Tudo bem?')
 */
 var c = 1
+/* estrutura de repetição com teste logico no inicio
 while (c <= 6) {
     console.log('tudo bem?')
     c++ // mesma coisa que c=c+1
 }
+//or
+while (c <= 6){
+    console.log(`Passo ${c}`)
+}
+*/
+
+// estrutura de repetição com teste logico no final
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 6)
